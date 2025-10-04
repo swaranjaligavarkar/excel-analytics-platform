eda17e67b4ef193050ca2505d317e3130df14706
swaranjaligavarkar <swaranjaligavarkar2002@gmail.com>
2025-10-04 16:03:15 +0530

    Add files via upload

---
+++ backend_server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
